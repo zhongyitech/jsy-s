5c19db72e9871630ffcfd03eb9938144923e09a3
lioa <lisz@zhongyitech.com>
2015-03-12 13:03:41 +0800

    当前用户

--- grails-app/resources/com/jsy/auth/UserCollectionResource.groovy
+++ grails-app/resources/com/jsy/auth/UserCollectionResource.groovy
@@ -2,8 +2,8 @@ package com.jsy.auth
 
 import com.jsy.fundObject.Finfo
 import com.jsy.system.Department
-import com.jsy.system.DepartmentCollectionResource
-import com.jsy.system.DepartmentResourceService
+import com.jsy.system.TypeConfig
+import com.jsy.utility.JsonResult
 import grails.converters.JSON
 import org.codehaus.groovy.grails.web.json.JSONObject
 import org.json.JSONArray
@@ -23,8 +23,8 @@ import javax.ws.rs.core.Response
 import static org.grails.jaxrs.response.Responses.ok
 
 @Path('/api/user')
-@Consumes(['application/xml','application/json'])
-@Produces(['application/xml','application/json'])
+@Consumes(['application/xml', 'application/json'])
+@Produces(['application/xml', 'application/json'])
 class UserCollectionResource {
     public static final Integer RESPONSE_STATUS_SUC = 200;
     public static final String REST_STATUS_SUC = "suc";
@@ -35,10 +35,9 @@ class UserCollectionResource {
 
     //创建用户
     @PUT
-    @Path("/create")
-    Response create(User dto,@QueryParam('rolelist') String rolelist) {
-        JSONObject result = new JSONObject();
-        String restStatus = REST_STATUS_SUC;
+    Response create(User dto, @QueryParam('rolelist') String rolelist) {
+//        JSONObject result = new JSONObject();
+//        String restStatus = REST_STATUS_SUC;
         def dd
         JSONObject jsdd = new JSONObject()
         JSONObject rlist = new JSONObject()
@@ -46,43 +45,46 @@ class UserCollectionResource {
             dd = userResourceService.create(dto)
             print(dd.properties)
             JSONArray ja = new JSONArray(rolelist)
-            print("ja.size = "+ja.length())
-            for (int i = 0;i<ja.length();i++){
+            print("ja.size = " + ja.length())
+            for (int i = 0; i < ja.length(); i++) {
                 def r
                 r = Role.get(ja.get(i).getAt("id"))
-                UserRole.create(dd,r).save(failOnError: true)
-                rlist.put(""+i+"",r)
+                UserRole.create(dd, r).save(failOnError: true)
+                rlist.put("" + i + "", r)
             }
-            jsdd.put("User",dd)
-            jsdd.put("Role",rlist)
-            result.put("rest_status", restStatus)
-            result.put("rest_result", jsdd as JSON)
-            print("return successful" + result.toString())
-            return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
-            print("return successful")
-        }catch (Exception e){
+            jsdd.put("User", dd)
+            jsdd.put("Role", rlist)
+//            result.put("rest_status", restStatus)
+//            result.put("rest_result", jsdd as JSON)
+//            print("return successful" + result.toString())
+//            return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
+//            print("return successful")
+
+            ok JsonResult.success(jsdd)
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI
             print(e)
             result.put("rest_status", restStatus)
             result.put("rest_result", dd as JSON)
             print("return failed")
-            return Response.ok(result.toString()).status(500).build()
+//            return Response.ok(result.toString()).status(500).build()
+            ok JsonResult.error(e.message)
         }
     }
 
     @GET
     @Path('/getUser')
     Response getUser() {
-        ok authorityService.getAuth(User.list(),'User').toString()
-//        ok(springSecurityService.getCurrentUser())
+//        ok authorityService.getAuth(User.list(), 'User').toString()
+        ok(springSecurityService.getCurrentUser())
     }
 
     //删除用户
     @DELETE
     @Path('/{id}')
     Response delete(@PathParam('id') Long id) {
-        boolean result=userResourceService.delete(id)
-        ok('{result:'+result+'}')
+        boolean result = userResourceService.delete(id)
+        ok('{result:' + result + '}')
     }
     //更改用户信息
 //    @PUT
@@ -101,8 +103,8 @@ class UserCollectionResource {
         ArrayList userList = new ArrayList()
         try {
             ia = userResourceService.readAll()
-            int i=0
-            ia.each{
+            int i = 0
+            ia.each {
                 userAndRole = new JSONObject(((it as JSON).toString()))
                 def ur = UserRole.findAllByUser(it)
                 int j = 0
@@ -110,13 +112,13 @@ class UserCollectionResource {
                 ur.each {
                     roleList.put(j, it.role)
                 }
-                userAndRole.put("Role",roleList)
+                userAndRole.put("Role", roleList)
                 userList.add(userAndRole)
             }
             result.put("rest_status", restStatus)
             result.put("rest_result", userList as JSON)
             return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
-        }catch (Exception e){
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI
             print(e)
             e.printStackTrace()
@@ -130,18 +132,18 @@ class UserCollectionResource {
 
     @GET
     @Path('/nameLike')
-    Response findByNameLike(@QueryParam('params') String username){
-        def users=User.findAllByUsernameLikeOrChainNameLike("%"+username+"%","%"+username+"%")
-        JSONArray jsonArray=new JSONArray()
+    Response findByNameLike(@QueryParam('params') String username,@QueryParam('deparmentid') int id) {
+        def users = User.findAllByUsernameLikeOrChainNameLike("%" + username + "%", "%" + username + "%")
+        JSONArray jsonArray = new JSONArray()
         users.each {
-            JSONObject jso=new JSONObject()
-            jso.put("value",it.chainName)
-            jso.put("data",it.id)
+            JSONObject jso = new JSONObject()
+            jso.put("value", it.chainName)
+            jso.put("data", it.id)
             jsonArray.put(jso)
         }
-        JSONObject jsonObject=new JSONObject()
-        jsonObject.put("query","Unit")
-        jsonObject.put("suggestions",jsonArray)
+        JSONObject jsonObject = new JSONObject()
+        jsonObject.put("query", "Unit")
+        jsonObject.put("suggestions", jsonArray)
 
         ok jsonObject.toString()
     }
@@ -149,76 +151,112 @@ class UserCollectionResource {
 
     @GET
     @Path('/username({username})')
-    Response findByName(@PathParam('username') String username){
+    Response findByName(@PathParam('username') String username) {
         ok userResourceService.findByName(username)
     }
 
     @Path('/{id}')
     UserResource getResource(@PathParam('id') Long id) {
-        new UserResource(userResourceService: userResourceService, id:id)
+        new UserResource(userResourceService: userResourceService, id: id)
     }
+
+    /**
+     * 获取职能为销售的部门列表
+     * @return 部门列表
+     */
+    @GET
+    @Path('/allDepartmentLader')
+    Response allDepartmentLeader() {
+        try {
+            //职能为销售部门
+            def typeconfig = TypeConfig.findByTypeAndMapValue(8,2)
+            def dep = Department.findAllByPerformance(typeconfig);
+            def result = new ArrayList<User>();
+            dep.each {
+                result.add(it.leader);
+            }
+            ok JsonResult.success(result)
+        }
+        catch (Exception e) {
+            print(e)
+            ok JsonResult.error(e.message)
+        }
+    }
+
+    /**
+     * 返回所有的部门经理列表（销售职能的部门）
+     *
+     * @param resource a Grails domain object.
+     * @return JAX-RS response.
+     */
     @GET
     @Path('/findUserFromRole')
-    Response findUserFromRole(@QueryParam('authority') String authority){
-        JSONObject result = new JSONObject();
-        String restStatus = REST_STATUS_SUC;
+    Response findUserFromRole(@QueryParam('authority') String authority) {
         def users
-        try{
-            if (null == authority || "" == authority){
+        try {
+            //Thread.sleep(5000);
+            if (null == authority || "" == authority) {
                 users = userResourceService.readAll()
-            }else {
-                Role role=Role.findByAuthority(authority)
-                users=UserRole.findAllByRole(role).collect{it.user}
+            } else {
+                Role role = Role.findByAuthority(authority)
+                users = UserRole.findAllByRole(role).collect { it.user }
             }
+            ok JsonResult.success(users)
 
-        }catch (Exception e){
-            restStatus = REST_STATUS_FAI
+        } catch (Exception e) {
             print(e)
+            ok JsonResult.error(e.message)
         }
-
-        result.put("rest_status", restStatus)
-        result.put("rest_result", users as JSON)
-        return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
+//        result.put("rest_status", restStatus)
+//        result.put("rest_result", users as JSON)
+//        return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
     }
+
     @GET
     @Path('/findUserFromRoleAndDepartment')
-    Response findUserFromRoleAndDepartment(@QueryParam('authority') String authority,@QueryParam('departmentid') String departmentid){
-        JSONObject result = new JSONObject();
-        String restStatus = REST_STATUS_SUC;
+    Response findUserFromRoleAndDepartment(
+            @QueryParam('authority') String authority, @QueryParam('departmentid') String departmentid) {
+        def result;
         def users
-        try{
+        try {
 
-            Role role=Role.findByAuthority(authority)
-            users=UserRole.findAllByRole(role).collect{it.user}
-            if([] == users){
+            Role role = Role.findByAuthority(authority)
+            users = UserRole.findAllByRole(role).collect { it.user }
+            if ([] == users) {
                 print("找不到该用户！")
             }
             print("users = " + users)
             def dp = Department.get(departmentid)
             print("dp.id = " + dp.id)
             print(users.department.id)
-            def user = User.findByDepartment( dp)
+            def user = User.findByDepartment(dp)
             users = user.find(users)
             print(users)
+            result = JsonResult.success(user)
 
-        }catch (Exception e){
-            restStatus = REST_STATUS_FAI
+        } catch (Exception e) {
             print(e)
+            result = JsonResult.error(e.message)
         }
-
-        result.put("rest_status", restStatus)
-        result.put("rest_result", users as JSON)
-        return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
+//        result.put("rest_status", restStatus)
+//        result.put("rest_result", users as JSON)
+        return Response.ok(result)
     }
+
+    /**
+     * 获取指定的用户
+     * @param uid
+     * @return 返回用户对象
+     */
     @GET
     @Path('/findUser')
-    Response findUser(@QueryParam('uid') String uid){
+    Response findUser(@QueryParam('uid') String uid) {
         JSONObject result = new JSONObject();
         String restStatus = REST_STATUS_SUC;
         def users
         JSONObject role = new JSONObject();
         JSONObject userAndRole = new JSONObject();
-        try{
+        try {
 
             users = User.get(uid)
             print(users.properties)
@@ -229,7 +267,7 @@ class UserCollectionResource {
             int i = 0
             ur.each {
                 i++
-                role.put(""+i+"", Role.get(it.role.id) as JSON)
+                role.put("" + i + "", Role.get(it.role.id) as JSON)
             }
             userAndRole.put("user", users as JSON)
             userAndRole.put("role", role)
@@ -238,7 +276,7 @@ class UserCollectionResource {
             return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
 
 
-        }catch (Exception e){
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI
             print(e)
             e.printStackTrace()
@@ -251,49 +289,60 @@ class UserCollectionResource {
 //        return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
     }
 
-
+    /**
+     * 获取指定用户的部门负责人
+     * @param uid
+     * @return User
+     */
     @GET
     @Path('/findUserLeader')
-    Response findUserLeader(@QueryParam('uid') String uid){
-        JSONObject result = new JSONObject();
-        String restStatus = REST_STATUS_SUC;
+    Response findUserLeader(@QueryParam('uid') String uid) {
+//        JSONObject result = new JSONObject();
+//        String restStatus = REST_STATUS_SUC;
         def users
-        JSONObject role = new JSONObject();
-        JSONObject userAndRole = new JSONObject();
+//        JSONObject role = new JSONObject();
+//        JSONObject userAndRole = new JSONObject();
         def leaders = [:]
-        try{
+        try {
 
             users = User.get(uid)
             print(users.properties)
             leaders.id = users.department.leader.id
             leaders.chainName = users.department.leader.chainName
             leaders.username = users.department.leader.username
-            result.put("rest_result", leaders as JSON)
-            result.put("rest_status", restStatus)
-            return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
+//            result.put("rest_result", leaders as JSON)
+//            result.put("rest_status", restStatus)
+//            return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
+            ok JsonResult.success(leaders)
 
-
-        }catch (Exception e){
-            restStatus = REST_STATUS_FAI
+        } catch (Exception e) {
+//            restStatus = REST_STATUS_FAI
             print(e)
             e.printStackTrace()
-            result.put("rest_status", restStatus)
-            return Response.ok(result.toString()).status(500).build()
+//            result.put("rest_status", restStatus)
+//            return Response.ok(result.toString()).status(500).build()
+            ok JsonResult.error(e.message)
         }
-
     }
 
-
-    @PUT
-    Response update(User dto,@QueryParam('id') Long id,@QueryParam('rolelist') String rolelist) {
-        print("id = "+id)
+    /**
+     * 更新用户信息
+     *
+     * @param dto
+     * @param id
+     * @param rolelist
+     * @return User 保存之后的用户信息
+     */
+    @POST
+    Response update(User dto, @QueryParam('id') Long id, @QueryParam('rolelist') String rolelist) {
+        print("id = " + id)
         JSONObject result = new JSONObject();
         String restStatus = REST_STATUS_SUC;
         def dd
         JSONObject jsdd = new JSONObject()
         JSONObject rlist = new JSONObject()
         try {
-            dd=userResourceService.update(dto,id)
+            dd = userResourceService.update(dto, id)
             def ur = UserRole.findAllByUser(User.get(id))
             if (ur) {
                 ur.each {
@@ -304,29 +353,29 @@ class UserCollectionResource {
             }
 
             JSONArray ja = new JSONArray(rolelist)
-            print("ja.size = "+ja.length())
-            for (int i = 0;i<ja.length();i++){
+            print("ja.size = " + ja.length())
+            for (int i = 0; i < ja.length(); i++) {
                 def r
                 r = Role.get(ja.get(i).getAt("id"))
-                print("r = "+r)
-                if (UserRole.exists(dd.id,r.id)){
-                    print("UserRole: "+dd+" and "+r+" allready exist!")
+                print("r = " + r)
+                if (UserRole.exists(dd.id, r.id)) {
+                    print("UserRole: " + dd + " and " + r + " allready exist!")
 //                    UserRole.create(dd,r,true).save(failOnError: true)
-                }else {
-                    print(UserRole.findByUserAndRole(dd, r)+" "+UserRole.exists(dd.id,r.id))
+                } else {
+                    print(UserRole.findByUserAndRole(dd, r) + " " + UserRole.exists(dd.id, r.id))
 //                    print("UserRole: "+dd+" and "+r+" allready exist!")
-                    UserRole.create(dd,r)//.save(failOnError: true)
+                    UserRole.create(dd, r)//.save(failOnError: true)
                 }
 
-                rlist.put(""+i+"",r as JSON)
+                rlist.put("" + i + "", r as JSON)
             }
-            jsdd.put("User",dd as JSON)
-            jsdd.put("Role",rlist)
+            jsdd.put("User", dd as JSON)
+            jsdd.put("Role", rlist)
 
             result.put("rest_status", restStatus)
             result.put("rest_result", jsdd)
             return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
-        }catch (Exception e){
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI
             e.printStackTrace()
             result.put("rest_status", restStatus)
@@ -337,7 +386,7 @@ class UserCollectionResource {
 
     @POST
     @Path('/readAllForPage')
-    Response readAllForPage(Finfo finfo){
+    Response readAllForPage(Finfo finfo) {
         print("userCollectionResource.readAllForPage()")
         org.json.JSONObject result = new org.json.JSONObject();
         String restStatus = REST_STATUS_SUC;
@@ -354,7 +403,7 @@ class UserCollectionResource {
             result.put("rest_total", total)
 
             return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
-        }catch (Exception e){
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI;
             print(e)
             e.printStackTrace()
@@ -372,9 +421,15 @@ class UserCollectionResource {
 
     }
 
+    /**
+     * 更新用户的密码，只对dto的密码字段进行更新。
+     *
+     * @param dto
+     * @return User
+     */
     @POST
     @Path('/updatePassword')
-    Response updatePassword(User dto){
+    Response updatePassword(User dto) {
         print("userCollectionResource.updatePassword()")
         org.json.JSONObject result = new org.json.JSONObject();
         String restStatus = REST_STATUS_SUC;
@@ -387,7 +442,7 @@ class UserCollectionResource {
             result.put("rest_result", obj as JSON)
 
             return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
-        }catch (Exception e){
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI
             e.printStackTrace()
             print(e)
@@ -395,8 +450,5 @@ class UserCollectionResource {
 
             return Response.ok(result.toString()).status(500).build()
         }
-
-
     }
-
 }
