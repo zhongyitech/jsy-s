c5d7e6c07c64bd37b804cc754eb2055c06a8d1fa
lioa <lisz@zhongyitech.com>
2015-03-12 14:40:41 +0800

    部分方法替换为新的数据返回方式

--- grails-app/resources/com/jsy/system/TypeConfigCollectionResource.groovy
+++ grails-app/resources/com/jsy/system/TypeConfigCollectionResource.groovy
@@ -1,5 +1,6 @@
 package com.jsy.system
 
+import com.jsy.utility.JsonResult
 import grails.converters.JSON
 import org.codehaus.groovy.grails.web.json.JSONObject
 
@@ -11,13 +12,12 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.Produces
 import javax.ws.rs.Path
-import javax.ws.rs.PathParam
 import javax.ws.rs.POST
 import javax.ws.rs.core.Response
 
 @Path('/api/typeConfig')
-@Consumes(['application/xml','application/json'])
-@Produces(['application/xml','application/json'])
+@Consumes(['application/xml', 'application/json'])
+@Produces(['application/xml', 'application/json'])
 class TypeConfigCollectionResource {
     public static final Integer RESPONSE_STATUS_SUC = 200;
     public static final String REST_STATUS_SUC = "suc";
@@ -33,11 +33,6 @@ class TypeConfigCollectionResource {
 
     @GET
     Response readAll() {
-//        MailBean mailBean=new MailBean()
-//        mailBean.mailTo="my061830@163.com"
-//        mailBean.subject="标题"
-//        mailBean.content="111123"
-//        sendMailService.toMail(mailBean)
         JSONObject result = new JSONObject();
         String restStatus = REST_STATUS_SUC;
         def tc
@@ -46,39 +41,30 @@ class TypeConfigCollectionResource {
             result.put("rest_status", restStatus)
             result.put("rest_result", tc as JSON)
             return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
-        }catch (Exception e){
+        } catch (Exception e) {
             restStatus = REST_STATUS_FAI
             print(e.stackTrace)
             result.put("rest_status", restStatus)
             result.put("rest_result", tc as JSON)
             return Response.ok(result.toString()).status(500).build()
         }
-//        result.put("rest_status", restStatus)
-//        result.put("rest_result", tc as JSON)
-//        return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
     }
 
     @Path('/{id}')
     TypeConfigResource getResource(@QueryParam('id') Long id) {
-        new TypeConfigResource(typeConfigResourceService: typeConfigResourceService, id:id)
+        new TypeConfigResource(typeConfigResourceService: typeConfigResourceService, id: id)
     }
 
     @GET
     @Path('/type')
-    Response findByType(@QueryParam('type') int type){
-        JSONObject result = new JSONObject();
-        String restStatus = REST_STATUS_SUC;
+    Response findByType(@QueryParam('type') int type) {
         def tc
         try {
             tc = typeConfigResourceService.findByType(type)
-        }catch (Exception e){
-            restStatus = REST_STATUS_FAI
+            ok JsonResult.success(tc)
+        } catch (Exception e) {
             print(e.stackTrace)
+            ok JsonResult.error(e.message)
         }
-//        print("rest_status")
-        result.put("rest_status", restStatus)
-//        print(result.get("rest_status"))
-        result.put("rest_result", tc as JSON)
-        return Response.ok(result.toString()).status(RESPONSE_STATUS_SUC).build()
     }
 }
