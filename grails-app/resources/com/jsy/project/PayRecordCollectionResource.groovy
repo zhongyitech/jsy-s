45d47bf005e97451bf2144851f7a684009c7995c
oswaldl <oswaldl@126.com>
2015-03-12 14:19:34 +0800

    finish receive records

--- grails-app/resources/com/jsy/project/PayRecordCollectionResource.groovy
+++ grails-app/resources/com/jsy/project/PayRecordCollectionResource.groovy
@@ -67,6 +67,8 @@ class PayRecordCollectionResource {
             TSProject project = TSProject.get(obj.project)
             BankAccount bankAccount = BankAccount.get(obj.bankselect)
             def paydate = Date.parse("yyyy-MM-dd", obj.paydate)
+
+
             PayRecord dto = new PayRecord(payDate:paydate,amount:obj.paytotal,payType:obj.moneyUseType,project:project,fund:fund,bankAccount:bankAccount);
             payRecordResourceService.create(dto)
 
@@ -99,37 +101,28 @@ class PayRecordCollectionResource {
             pay_record.payDate = payRecord.payDate
 
             pay_record.id = payRecord.id
-            pay_record.manage_pay = payRecord.amount * project.manage_per                       //管理费
-            pay_record.community_pay = payRecord.amount * project.community_per                 //渠道费
-            pay_record.borrow_pay = payRecord.amount * project.borrow_per                       //借款
-            pay_record.penalty_pay = payRecord.amount * project.penalty_per                     //违约金
+            pay_record.manage_pay = payRecord.manage_bill                       //管理费
+            pay_record.community_pay = payRecord.community_bill                 //渠道费
+            pay_record.borrow_pay = payRecord.borrow_bill                       //借款
+
             pay_record.interest_year1 = project.year1
-            pay_record.interest_pay = payRecord.amount * project.interest_per * project.year1   //第一年利率
+            pay_record.interest_pay = payRecord.interest_bill   //第一年利率
             pay_record.dateCount=dayDifferent(payRecord.payDate,new Date())                     //投资天数
+
+
 //            singleCount", "costCount", "dayCount
 
             //这里开始计算逾期利息
             Date nowDate = new Date()
             Date lastDate = addYears(payRecord.payDate,Integer.parseInt(new java.text.DecimalFormat("0").format((project.year1 + project.year2))))
 
-            if(nowDate.after(lastDate)){//判断超出预定时间
-                def owe_money = payRecord.amount - payRecord.payMainBack
-                def over_days = dayDifferent(lastDate,nowDate)
-                if(owe_money > 0){
-                    if("singleCount".equals(project.interestType)){//单利：欠款*interest_per/365*超出的天数
-                        pay_record.over_interest_pay = (owe_money * project.interest_per * over_days / 365)
-                    }else if("costCount".equals(project.interestType)){//单利：(欠款+欠款*interest_per)*penalty_per/365*超出的天数
-                        pay_record.over_interest_pay = (owe_money * (1+project.interest_per) * over_days / 365)
-                    }else if("dayCount".equals(project.interestType)){//复利：便历每一天，做加法：第一天:(欠款+欠款*penalty_per)*penalty_per/365*1 ,第二天：第一天的利息*penalty_per/365*1，如此类推
-                        pay_record.over_interest_pay=(owe_money * (1+project.interest_per) / 365);  //第一天
-                        for(int i=1;i<over_days;i++){//第二天起
-                            pay_record.over_interest_pay += (pay_record.over_interest_pay * (1+project.interest_per) / 365);
-                        }
-                    }
-                }
-            }else{
-                pay_record.over_interest_pay = 0
+            if(nowDate.after(lastDate) && !payRecord.isOverDate){//判断超出预定时间，查询时发现超出，就修改penalty_pay
+                pay_record.penalty_pay = payRecord.amount * project.penalty_per                     //违约金
+                payRecord.isOverDate=true
+                payRecord.penalty_bill=pay_record.penalty_pay
+                payRecord.save(failOnError: true)
             }
+            pay_record.over_interest_pay = payRecord.getOverDue()
             rtn << pay_record
         }
 
