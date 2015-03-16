263eac7294e65de0b794b85328fd780083f8569a
weizhansheng <weizhansheng@outlook.com>
2015-03-16 18:28:29 +0800

    增强反射查询

--- grails-app/services/com/jsy/system/ReflectService.groovy
+++ grails-app/services/com/jsy/system/ReflectService.groovy
@@ -1,5 +1,6 @@
 package com.jsy.system
 
+import com.jsy.auth.User
 import com.jsy.utility.JsonResult
 import grails.transaction.Transactional
 
@@ -10,14 +11,55 @@ class ReflectService {
         try {
             //不指定字段是否为空
             //if ("".equals(fields)) return JsonResult.success([:])
-            def obj = Class.forName(className).newInstance().get(id)
-            if(!obj)return JsonResult.success([:])
-            def fieldArray = fields.split(",")
-            def result = [:]
-            fieldArray.each { field -> if(obj.metaClass.hasProperty(obj,field)) result[field] = obj[field]}
-            return JsonResult.success(result.isEmpty() ? obj : result)
+            def domain=Class.forName(className).newInstance()
+            def ids=[]
+            if(id&&id.contains(",")){
+                id.split(',').each {
+                    tmpId->
+                        def tmp=castLong(tmpId)
+                        if(tmp)ids.push(tmp)
+                }
+            }else{
+                def tmpId=castLong(id)
+                if(tmpId) ids.push(tmpId)
+            }
+            if(ids.isEmpty()) return JsonResult.success([])
+            def obj= domain.withCriteria { inList('id',ids)}
+            if(!obj) return JsonResult.success([])
+            def result = []
+            def fieldArray=fields.split(',')
+            obj.each {
+                item->
+                    result.push(filterProp(item,fieldArray))
+            }
+            return JsonResult.success(result)
         } catch (Exception e) {
             return JsonResult.error(e.message)
         }
     }
+
+    def castLong(String obj){
+        try {
+            return Long.parseLong(obj)
+        }catch (e){
+            return null
+        }
+    }
+
+    /**
+     * 字段过滤
+     * @param obj
+     * @param fieldArray
+     * @return
+     */
+    def filterProp(def obj,Object[] fieldArray){
+        def result=[:]
+        fieldArray.each {
+            field ->
+                if(obj.metaClass.hasProperty(obj,field))
+                    result[field] = obj[field]
+        }
+        return result.isEmpty() ? obj : result
+    }
+
 }
