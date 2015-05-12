8a6a6434903139327287eb930eceff5c714d9307
lioa <lisz@zhongyitech.com>
2015-05-12 16:13:32 +0800

    添加 注释

--- grails-app/services/com/jsy/archives/INVESTMENT_STATUS.groovy
+++ grails-app/services/com/jsy/archives/INVESTMENT_STATUS.groovy
@@ -1,18 +1,20 @@
 package com.jsy.archives
 
 /**
+ *   Ãÿ ‚…Í«Î◊¥Ã¨ ∂±¬Î
  * Created by lioa on 2015/4/21.
  */
 enum INVESTMENT_STATUS {
-    New(0),Normal(1),BackUp(2)
-    private  int _value
+    New(0), Normal(1), BackUp(2)
+    private int _value
 
     INVESTMENT_STATUS(int _value) {
         this._value = _value
     }
-    public boolean eq(int value){
-        return  this._value==value
+
+    public boolean eq(int value) {
+        return this._value == value
     }
 
-    int getValue() {return  _value}
+    int getValue() { return _value }
 }
