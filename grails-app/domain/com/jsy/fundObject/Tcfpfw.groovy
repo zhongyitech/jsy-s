e0fc815baa2f1f05b93d6517e06f70d20a5a0b26
lioa <lisz@zhongyitech.com>
2015-05-15 15:22:08 +0800

    添加"税率"和提成方式(税前,税后)

--- grails-app/domain/com/jsy/fundObject/Tcfpfw.groovy
+++ grails-app/domain/com/jsy/fundObject/Tcfpfw.groovy
@@ -7,14 +7,19 @@ class Tcfpfw {
     //部门经理
     int manageerId
     //业务提成
-    double  businessCommision
+    double businessCommision
     //管理提成
-    double  manageCommision
+    double manageCommision
 
+    //该部门的税率
+    double rate
+
+    //提成方式( true:税前/ false:税后
+    boolean rateBefore = false
     //是否包销
-    boolean allSell=false
+    boolean allSell = false
     //包销收益率
-    double investment=0
+    double investment = 0
 
     static constraints = {
     }
