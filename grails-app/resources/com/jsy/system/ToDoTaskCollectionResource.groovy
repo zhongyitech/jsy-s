15465744dea53430e7249cf222800903136dadcb
lioa <lisz@zhongyitech.com>
2015-05-29 17:27:42 +0800

    待办事项获取代码，只获取自己的

--- grails-app/resources/com/jsy/system/ToDoTaskCollectionResource.groovy
+++ grails-app/resources/com/jsy/system/ToDoTaskCollectionResource.groovy
@@ -3,6 +3,8 @@ package com.jsy.system
 import com.jsy.auth.User
 import com.jsy.auth.UserRole
 import com.jsy.fundObject.Finfo
+import com.jsy.utility.DomainHelper
+import com.jsy.utility.MyResponse
 import grails.converters.JSON
 import org.json.JSONObject
 
@@ -26,49 +28,43 @@ class ToDoTaskCollectionResource {
     def toDoTaskResourceService
     def springSecurityService
 
-    @POST
-    Response create(ToDoTask dto) {
-        created toDoTaskResourceService.create(dto)
-    }
-
     //查询所有待办任务
     @POST
+    @Path('/all')
+    Response readAll(Map arg) {
+        MyResponse.page {
+            DomainHelper.getPage(ToDoTask,arg)
+        }
+    }
+     //查询所有待办任务
+    @POST
     @Path('/getTodo')
-    Response readAll(Finfo finfo) {
-        User user=springSecurityService.getCurrentUser()
-        //取用户所有的角色
-        def roles=UserRole.findAllByUser(user).collect {it.role}
-        //返回的任务为未完成状态的
-        def toDoTasks=ToDoTask.findAllByCljsInListAndStatus(roles,0,[max: finfo.pagesize,sort:"cjsj", order:"asc", offset:finfo.startposition])
-        int total=ToDoTask.findAllByCljsInListAndStatus(roles,0).size()
-        JSONObject result = new JSONObject();
-        result.put("rest_status", "suc")
-        result.put("rest_result", toDoTasks as JSON)
-        result.put("rest_total", total)
-        return Response.ok(result.toString()).status(200).build()
+    Response getTodo(Map arg) {
+        MyResponse.page {
+            DomainHelper.getPage(ToDoTask,arg)
+        }
     }
 
     //查询待办任务个数
     @GET
     @Path('/getTotal')
     Response readAllTotal() {
-        User user=springSecurityService.getCurrentUser()
-        def roles=UserRole.findAllByUser(user).collect {it.role}
-        //返回的任务为未完成状态的
-        def toDoTasks=ToDoTask.findAllByCljsInListAndStatus(roles,0)
-        ok toDoTasks.size()
+        MyResponse.ok {
+            User user = springSecurityService.getCurrentUser()
+            def roles = UserRole.findAllByUser(user).collect { it.role }
+            //返回的任务为未完成状态的
+            def toDoTasks = ToDoTask.findAllByCljsInListAndStatus(roles, 0)
+            ok toDoTasks.size()
+        }
     }
 
 
     @GET
     @Path('/getById')
     Response getTodo(@QueryParam('id') Long id) {
-        JSONObject result = new JSONObject();
-        result.put("rest_status", 200)
-        result.put("rest_result", ToDoTask.get(id) as JSON)
-        return Response.ok(result.toString()).status(200).build()
-
-//        new ToDoTaskResource(toDoTaskResourceService: toDoTaskResourceService, id: id)
+        MyResponse.ok{
+            ToDoTask.get(id)
+        }
     }
 
     @Path('/{id}')
